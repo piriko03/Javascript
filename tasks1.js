1d2b20fff1bac701efd2bf1a25bb6962df07027d
Mariam Pirosmanishvili <146012625+piriko03@users.noreply.github.com>
2024-08-02 22:40:04 +0400

    Add files via upload

---
+++ tasks1.js
@@ -0,0 +1,53 @@
+//1.a program that prints "Hello World".
+//console.log("Hello World")
+
+//2.create a variable and print its value.
+//let number = 5
+//console.log(number)
+
+//3.program to add two numbers and print the result.
+//let num1 = 5
+//let num2 = 7
+//let result = num1 + num2
+//console.log(result)
+
+//4.a program that subtracts two numbers and displays the result.
+//let num1 = 10
+//let num2 = 7
+//let result = num1 - num2
+//console.log(result)
+
+//5.a program to multiply two numbers and print the result.
+//let num1 = 8
+//let num2 = 9
+//let result = num1 * num2
+//console.log(result)
+
+//6.a program to divide two numbers and print the result.
+//let num1 = 54
+//let num2 = 6
+//let result = num1 / num2
+//console.log(result)
+
+//7.a program to calculate the remainder of the division of two numbers.
+//let num1 = 10
+//let num2 = 7
+//let result = num1 % num2
+//console.log(result)
+
+//8.a variable and assign it a string. Print the length of the string.
+//let myname = "mariam pirosmanishvili"
+//console.log(myname.length)
+
+//9.a program to concatenate two strings and display the result.
+//let myname = "mariam "
+//let mysurname = "pirosmanishvili"
+//console.log(myname + mysurname)
+
+//10.a program to convert a string to upper case.
+//let myname = "mariam"
+//console.log(myname.toUpperCase())
+
+//11.a program to convert a string to lower case.
+//let myname = "MARIAM"
+//console.log(myname.toLowerCase())
