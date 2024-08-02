7b997f205a4c005b2ef39112de6b31ec2cc0ab49
Mariam Pirosmanishvili <146012625+piriko03@users.noreply.github.com>
2024-08-02 22:45:26 +0400

    Add files via upload

---
+++ tasks2.js
@@ -0,0 +1,40 @@
+//12.odd or even
+//let num = 9;
+//if (num%2==0){
+//    console.log("even")
+//} else {
+//    console.log("odd")
+//}
+
+//13. positive,negative or null
+//let num = 0;
+//if (num > 0){
+//    console.log("positive")
+//}else if (num < 0){
+//    console.log("negative")
+//}else console.log("null")
+
+//14. max
+//let num1 = 9;
+//let num2 = 9;
+//if (num1 > num2){
+//    console.log(num1 , "more")
+//} else if (num2 > num1){
+//    console.log(num2 , "less")
+//}else console.log("equals")
+
+//15. min
+//let num1 = 9;
+//let num2 = 9;
+//if (num1 < num2){
+//    console.log(num1 , "less")
+//} else if (num2 < num1){
+//    console.log(num2 , "more")
+//}else console.log("equals")
+
+//16. Is it divided or not?
+//let num1 = 81;
+//let num2 = 10;
+//if (num1 % num2 == 0){
+//    console.log("yes")
+//}else console.log("no")
