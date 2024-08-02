84683c3f43f8f7bce3d06b0abd31b8f9363f979f
Mariam Pirosmanishvili <146012625+piriko03@users.noreply.github.com>
2024-08-02 22:42:15 +0400

    Add files via upload

---
+++ strings.js
@@ -0,0 +1,59 @@
+//---------------------- string methods -------------------
+
+//includes
+//let str = "bitcamp front-end";
+//console.log(str.includes("bitcamp"));
+
+//startsWith , endsWith
+//let str = "bitcamp front-end";
+//let check = str.startsWith("bitcamp");
+//console.log(check);
+
+//let str = "bitcamp front-end";
+//let check = str.endsWith("end");
+//console.log(check);
+
+//let str = "madrewam@gmail.com";
+//let check = str.endsWith("@gmail.com");
+//console.log(check);
+
+//replace
+//let str = "hello world";
+//let check = str.replace("hello" , "bye");
+//console.log(check);
+
+//index
+//let str = "hello world";
+//let index = str[str.length - 1];
+//console.log(index);
+
+//slice (substring)
+//let str = "hello world";
+//let check = str.slice(0,4);
+//console.log(check);
+
+//concat
+//let str = "hello";
+//let str2 = "world";
+//let check = str.concat(" world");
+//console.log(check);
+
+//trim
+//let str = "   mari   "
+//let check = str.trim();
+//let check2 = str.trimStart();
+//let check3 = str.trimEnd();
+//console.log(check);
+//console.log(check2);
+//console.log(check3);
+
+//padStart , padEnd
+//let num = 5;
+//let string = num.toString();
+//let padStart = string.padStart(2 , 0);
+//console.log(padStart);
+
+//check if string's first character is uppercase
+//let str = "Bitcamp";
+//let letter = str[0];
+//console.log(letter === letter.toUpperCase());
