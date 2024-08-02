bc84b85ef5610d1b31c6869ebf7de0d9bd815354
Mariam Pirosmanishvili <146012625+piriko03@users.noreply.github.com>
2024-08-02 22:51:11 +0400

    Add files via upload

---
+++ tasks3.js
@@ -0,0 +1,30 @@
+//1. Generate a random number between 0 and 1 and find out if it is less than, greater than or equal to 0.5
+//let num = Math.random(0,1);
+//console.log(num);
+//if (num>0.5) console.log("greater")
+//else if (num<0.5) console.log("less")
+//else console.log("equals")
+
+//2. Create a program that will find out whether a person has the right to vote
+//let age = 17;
+//if (age >= 18) console.log("yes")
+//else console.log("no")
+
+//3. Create a program that will find out a student's grade based on his grade
+//let scr = 34;
+//if (scr>90) console.log("A")
+//else if (scr>80) console.log("B")
+//else if (scr>70) console.log("C")
+//else if (scr>60) console.log("D")
+//else if (scr>50) console.log("E")
+//else console.log("F")
+
+//4. Is the number divisible by 3 and 5?
+//let num = 21;
+//if (num%3==0 && num%5==0) console.log("yes")
+//else console.log("no")
+
+//5. Is the number between 100 and 200?
+//let num = 100;
+//if (num>=100 && num<=200) console.log("yes")
+//else console.log("no")
